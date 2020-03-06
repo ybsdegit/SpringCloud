1c117aaa6e61434d8ee466a25fe9549d8dc95f8a
ybsdegit <ybsdeyx@163.com>
2020-03-06 17:15:11 +0800

    Eureka 集群配置

---
+++ springcloud-eureka-7003/src/main/java/com/ybs/springcloud/EurekaServer_7003.java
@@ -0,0 +1,20 @@
+package com.ybs.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * EurekaServer_7001
+ *
+ * @author Paulson
+ * @date 2020/3/6 0:33
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer_7003 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_7003.class, args);
+    }
+}
